f0db87f8f501e5a0b46223226cf388bc03a7ed25
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-04-15 17:08:57 -0400

    Added a for loop with nested if statements that check to see if the user has confirmed that they want uppercase letters, lowercase letters, numbers and special characters, and randomly chooses characters from the characters object until a password of the specified length is reached.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let passwordChar = ['6', '7'];
 
 // Gather user input and generate a password within the constraints of the user input
 function generatePassword () {
@@ -11,13 +10,33 @@ function generatePassword () {
   const specialChar = prompt("Would you like your new password to contain special characters, such as @, #, %, etc? y/n");
 
   const characters = {
-    letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    numbers: Math.floor(Math.random() * 9),
-    special: ["!", "\"", "#", "$", "%", "&", "\'", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"],
+    lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    numbers: ["0", "1", "2", "3", "4", "5", "6","7", "8", "9"],
+    special: ["\!", "\"", "\#", "\$", "\%", "\&", "\'", "\(", "\)", "\*", "\+", "\,", "\-", "\.", "\/", "\:", "\;", "\<", "\=", "\>", "\?", "\@", "\[", "\]", "\^", "\_", "\`", "\{", "\|", "\}", "\~"],
   }
+
+  let passwordChar = [];
+
+  for (x=0; x<passwordLength; x++) {
+    const type = Math.floor(Math.random() * 3);
+    if (lowerCase === 'y' && type === 0) {
+      const random = Math.floor(Math.random() * 25);
+      passwordChar.push(characters.lower[random]);
+    } else if (upperCase === 'y' && type === 1) {
+      const random = Math.floor(Math.random() * 25);
+      passwordChar.push(characters.upper[random]);
+    } else if (numeric === 'y' && type === 2) {
+      const random = Math.floor(Math.random() * 9);
+      passwordChar.push(characters.numbers[random]);
+    } else if (specialChar === 'y' && type === 3) {
+      const random = Math.floor(Math.random() * 30);
+      passwordChar.push(characters.letters[random]);
+    }
+  }
+  console.log(passwordChar);
+  console.log(passwordChar.join(""));
 }
-
-
 
 // Write password to the #password input
 function writePassword() {
